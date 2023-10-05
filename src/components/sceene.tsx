1bb785351f0be7881a796b29580d798282b41660
Andrés Marrugo <andrexmarrugo@gmail.com>
2023-10-05 01:57:48 -0500

    responsive

--- src/components/sceene.tsx
+++ src/components/sceene.tsx
@@ -97,17 +97,29 @@ const Sceene = (props: Props) => {
                 {props.children}
             </section>
             <section ref={boxRef} className='absolute flex flex-col items-center justify-center select-none top-0 h-full w-full bg-black overflow-hidden z-[100]'>
-                <h1 ref={nameRef} className="font-bold xl:text-9xl sm:text-6x">Andrés Marrugo</h1>
-                <h2 ref={titleRef} className="text-4xl">Frontend Developer</h2>
+                <h1 ref={nameRef} className="font-bold text-5xl sm:text-9xl">Andrés Marrugo</h1>
+                <h2 ref={titleRef} className="text-xl sm:text-4xl">Frontend Developer</h2>
             </section>
             <section ref={boxRef2} className='absolute top-0 h-full w-full bg-blue-300 overflow-hidden z-[10]'>
-                <img src="https://picsum.photos/800/500?random=1" alt="random img 1" className='w-full max-w-full' />
+                <img
+                    src="https://picsum.photos/800/500?random=1"
+                    alt="random img 1"
+                    className="w-screen h-screen object-cover"
+                />
             </section>
             <section ref={boxRef3} className='absolute top-0 h-full w-full bg-red-300 overflow-hidden z-[5]'>
-                <img src="https://picsum.photos/800/500?random=2" alt="random img 2" className='w-full' />
+                <img
+                    src="https://picsum.photos/800/500?random=2"
+                    alt="random img 1"
+                    className="w-screen h-screen object-cover"
+                />
             </section>
             <section className='absolute top-0 h-full w-full bg-purple-300 overflow-hidden z-0'>
-                <img src="https://picsum.photos/800/500?random=3" alt="random img 3" className='w-full' />
+                <img
+                    src="https://picsum.photos/800/500?random=3"
+                    alt="random img 1"
+                    className="w-screen h-screen object-cover"
+                />
             </section>
         </div>
     )
